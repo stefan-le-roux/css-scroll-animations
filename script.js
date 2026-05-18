c9615323e3fb001f2212bf1756bc11ef053c004a
Stefan le Roux <167118808+stefan-le-roux@users.noreply.github.com>
2026-05-18 10:10:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const audio = document.getElementById('background-audio');
+const toggleButton = document.getElementById('play-button');
+
+toggleButton.addEventListener('change', () => {
+		if (audio.paused) {
+				audio.volume = 0.05; // Set volume
+				audio.play(); // Play audio
+		} else {
+				audio.pause(); // Pause audio
+		}
+});
