6cfff45ce5abf6ffcb89854d60e20a499048669b
Stefan le Roux <167118808+stefan-le-roux@users.noreply.github.com>
2026-06-01 13:13:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const toggleButton = document.getElementById('play-button');
 
 toggleButton.addEventListener('change', () => {
 		if (audio.paused) {
-				audio.volume = 0.05; // Set volume
+				audio.volume = 0.1; // Set volume
 				audio.play(); // Play audio
 		} else {
 				audio.pause(); // Pause audio
